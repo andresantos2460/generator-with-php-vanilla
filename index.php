5fd560f1da96abcf5b6ed53602b0ee38ba05c6ad
AndreSantos-2460 <andresantosv2004@gmail.com>
2024-11-10 17:55:49 +0000

    modal delete

--- index.php
+++ index.php
@@ -557,14 +557,25 @@ $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
                                           <div class="modal-dialog modal-dialog-centered">
                                               <div class="modal-content">
                                                   <div class="modal-header">
-                                                      <h3 class="modal-title">Modal title</h3>
+                                                      <h3 class="modal-title">Delete My Password</h3>
                                                       <div class="btn btn-icon btn-sm btn-active-light-primary ms-2" data-bs-dismiss="modal" aria-label="Close">
                                                           <i class="ki-duotone ki-cross fs-1"><span class="path1"></span><span class="path2"></span></i>
                                                       </div>
                                                   </div>
 
                                                   <div class="modal-body">
-                                               
+                                                  <div data-kt-password-meter="true">
+                                                   <div class="position-relative mb-3">
+                                                  <input class="form-control form-control-lg form-control-solid" type="password" placeholder="" name="new_password" autocomplete="off" />
+
+                                                  <!--begin::Visibility toggle-->
+                                                  <span class="btn btn-sm btn-icon position-absolute translate-middle top-50 end-0 me-n2" data-kt-password-meter-control="visibility">
+                                                    <i class="ki-duotone ki-eye-slash fs-1"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span></i>
+                                                    <i class="ki-duotone ki-eye d-none fs-1"><span class="path1"></span><span class="path2"></span><span class="path3"></span></i>
+                                                  </span>
+                                                  <!--end::Visibility toggle-->
+                                                </div>
+                                                </div>
                                                   </div>
                                                   <div class="modal-footer">
                                                       <button type="button" class="btn btn-light" data-bs-dismiss="modal">Close</button>
